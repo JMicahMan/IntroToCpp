352e9921ebc25bfb49123073b71742c2cb160ded
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:26:03 -0500

    Add project files.

---
+++ IntroToCpp/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Micah" << std::endl;
+	std::cout << "Blueberry" << std::endl;
+	std::cout << "Strawberry" << std::endl;
+	std::cout << "Orange" << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "1. Son of a Star\n";
+	std::cout << "2. Imperial Eagle\n";
+	std::cout << "3. Price of a Mile\n";
+	std::cout << "4. Hummingbird\n";
+	std::cout << "5. 1812 w_cannons\n";
+	std::cout << "6. Cotton Eye Joe\n";
+
+	std::cout << std::endl;
+
+	std::cout << "0\n";
+	std::cout << "1\n";
+	std::cout << "2\n";
+	std::cout << "3\n";
+	std::cout << "4\n";
+	std::cout << "5\n";
+
+	std::cout << std::endl;
+
+	std::cout << "5\n";
+	std::cout << "4\n";
+	std::cout << "3\n";
+	std::cout << "2\n";
+	std::cout << "1\n";
+	std::cout << "0\n";
+
+
+
+	system("Pause");
+	return 0;
+}
